7661ffdab7ead83dac95048e358ce44fd2f836e3
Adham Yacout <Adham.yacout@gmail.com>
2023-12-06 16:43:06 +0200

    added appbar

--- lib/Mainscreen.dart
+++ lib/Mainscreen.dart
@@ -5,8 +5,8 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Mainscreen extends StatefulWidget{
-  static String routename="Mainsceen";
+class Mainscreen extends StatefulWidget {
+  static String routename = "Mainsceen";
 
   @override
   State<Mainscreen> createState() => _MainscreenState();
@@ -15,8 +15,8 @@ class Mainscreen extends StatefulWidget{
 class _MainscreenState extends State<Mainscreen> {
   Random random = Random();
   final player = AudioPlayer();
-  int dicenum=1;
-  int dicenum2=1;
+  int dicenum = 1;
+  int dicenum2 = 1;
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
@@ -27,61 +27,84 @@ class _MainscreenState extends State<Mainscreen> {
     double btnWidth = screenWidth * 0.2;
     double fontSize = screenWidth * 0.05;
 
-  return Scaffold(
-    body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.blue,
-            Colors.lightBlueAccent,
-            Colors.white
-          ]
+    return Scaffold(
+      extendBodyBehindAppBar: true, // Allow body to extend behind the AppBar
+      appBar: AppBar(
+        backgroundColor: Colors.transparent, // Set background color to transparent
+        elevation: 0, // Remove AppBar elevation
+        centerTitle: true, // Center align the title
+        title: Text(
+          "Roll the Dice",
+          style: TextStyle(
+            fontSize: 40.0, // Adjust font size if needed
+            fontWeight: FontWeight.bold, // Adjust font weight if needed
+            color: Colors.white, // Set text color to white
+            shadows: [
+              Shadow(
+                offset: Offset(2, 2),
+                color: Colors.black,
+                blurRadius: 8,
+              ),
+            ],
+          ),
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment:MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(image: AssetImage('assets/images/dice-${dicenum}.png'),height: imageHeight,width: imageWidth,),
-                Image(image: AssetImage('assets/images/dice-${dicenum2}.png'),height: imageHeight,width: imageWidth,)
-              ],
-            ),
-            SizedBox(height: 50,),
-            ElevatedButton(onPressed: () {
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blue, Colors.lightBlueAccent, Colors.white]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(flex: 4,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/dice-${dicenum}.png'),
+                    height: imageHeight,
+                    width: imageWidth,
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/dice-${dicenum2}.png'),
+                    height: imageHeight,
+                    width: imageWidth,
+                  )
+                ],
+              ),
+              Spacer(flex: 3,),
+              ElevatedButton(
+                  onPressed: () {
+                    dicenum = random.nextInt(6) + 1;
+                    dicenum2 = random.nextInt(6) + 1;
+                    int total = dicenum + dicenum2;
 
-              dicenum = random.nextInt(6) + 1;
-              dicenum2 = random.nextInt(6) + 1;
-              int total=dicenum+dicenum2;
+                    setState(() {
+                      player.play(AssetSource('sounds/${total}.wav'));
+                    });
+                  },
+                  child: Container(
+                    height: btnHeight,
+                    width: btnWidth,
+                    child: Center(
+                      child: Text("Roll me",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: fontSize, shadows: [
+                            Shadow(
+                                offset: Offset(2, 2),
+                                color: Colors.black,
+                                blurRadius: 8)
+                          ])),
+                    ),
+                  )),
 
+            Spacer(flex: 2,)],
 
-               setState(() {
-                 player.play(AssetSource('sounds/${total}.wav'));
-
-
-               });
-            }, child: Container(height:btnHeight , width: btnWidth , child:
-            Center(
-              child: Text("Roll me",textAlign: TextAlign.center,
-                  style: TextStyle(
-                fontSize: fontSize,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2,2),
-                        color: Colors.black,
-                        blurRadius: 8
-                      )
-                    ]
-              )),
-            ),))
-
-
-          ],
+          ),
         ),
       ),
-    ),
-  );
+    );
   }
 }
